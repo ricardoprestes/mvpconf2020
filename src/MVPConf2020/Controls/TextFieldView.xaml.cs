71d680059c93ea01307fce2e782004002a4c0bcf
Ricardo Prestes <ricardo.logan@hotmail.com>
2020-12-07 14:28:42 -0300

    Criando controle

---
+++ src/MVPConf2020/Controls/TextFieldView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MVPConf2020.Controls
+{
+    public partial class TextFieldView : ContentView
+    {
+        public TextFieldView()
+        {
+            InitializeComponent();
+        }
+    }
+}
